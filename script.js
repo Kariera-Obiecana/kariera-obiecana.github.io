571b78e377f0368025c0ab53459e6d96e57537e3
balaboo123 <146384000+balaboo123@users.noreply.github.com>
2024-02-17 18:55:53 +0100

    Ikony, mobilna kompatybilnosc, ulepszenia

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+const socialsContainer = document.querySelector(".socials");
 const socials = document.querySelectorAll(".socials > *");
 
 const socialWindow = document.querySelector(".social-window");
 const socialIcon = document.querySelector(".social-window .social-icon");
 const socialName = document.querySelector(".social-window .title");
 const socialDesc = document.querySelector(".social-window .description");
+
+const visit = document.querySelector(".social-window .visit");
 
 const pages = document.querySelector(".selection-menu > a");
 // const selectedArrow = document.querySelector(".selection-menu .arrow");
@@ -26,20 +29,48 @@ let descriptions = {
     "Pinterest": "opis co na Pintereście"
 }
 
+let clicked = false;
+let currentLink = "";
+
+visit.addEventListener("click", e => {
+    window.open(currentLink, '_blank');
+})
+
+
+
+function disableSocial() {
+    console.log(clicked);
+    if (clicked) return;
+
+    visit.classList.add("hidden");
+
+    // Set border to disabled (transparent)
+    socialWindow.style.border = "0.35rem solid rgba(255,255,255,0.1)";
+    socialWindow.style.backgroundColor = "rgba(244,244,244, 0.1)"
+}
+
 socials.forEach(social => {
     if (social.classList.contains("arrow")) return;
 
     let link = social.dataset.link;
+    currentLink = link;
 
-    social.addEventListener("click", e => {
+    social.addEventListener("dblclick", e => {
         window.open(link, '_blank');
     })
+    
+
+    // social.addEventListener("click", e => {
+    //     window.open(link, '_blank');
+    // })
 
     let image = social.getAttribute("src");
     let title = social.getAttribute("alt");
     // currentSocial = title;
 
-    social.addEventListener("mouseover", e => {
+    function setSocial() {
+        visit.classList.remove("hidden");
+
         // Set icon and text
         socialIcon.src = image;
         socialName.innerText = `nasz ${title}`;
@@ -48,19 +79,25 @@ socials.forEach(social => {
         // Set border to enabled (less transparent)
         socialWindow.style.border = "0.35rem solid rgba(255,255,255, 0.9)";
         socialWindow.style.backgroundColor = "rgba(255,255,255, 0.45)"
-        // socialIcon.style.opacity = 1;
-        
-    })
+    }
 
-    social.addEventListener("mouseleave", e => {
-        // Set border to disabled (transparent)
-        socialWindow.style.border = "0.35rem solid rgba(255,255,255,0.1)";
-        socialWindow.style.backgroundColor = "rgba(244,244,244, 0.1)"
-        // socialIcon.style.opacity = 0.3;
-    })
+    social.addEventListener("mouseover", setSocial);
+    social.addEventListener("click", e => {
+        setSocial();
+        clicked = true;
+    }); // mobilka
+
+    social.addEventListener("mouseleave", disableSocial);
 
 })
 
+// Close (mobile)
+document.addEventListener("click", e => {
+    if (!socialWindow.contains(e.target) && !socialsContainer.contains(e.target)) {
+        clicked = false;
+        disableSocial();
+    }
+});
 
 
 // Switch 
